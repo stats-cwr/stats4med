fe938aec2640edc005e4d98ae1a5e2d86d4e12ac
Eduardo Junior <edujrrib@gmail.com>
2016-08-15 12:28:55 -0300

    Add files to travis-ci generate the website

---
+++ _deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git config --global user.email "edujrrib@gmail.com"
+git config --global user.name "Travis boot"
+
+git clone -b gh-pages https://${GIT_KEY}@github.com/${TRAVIS_REPO_SLUG}.git dir-tmp
+cd dir-tmp
+cp -r ../course/_site/* ./
+cp -r ../seminars/*.pdf ./
+git add --all *
+git commit -m "Atualização automática (travis build ${TRAVIS_BUILD_NUMBER})" || true
+git push origin gh-pages
