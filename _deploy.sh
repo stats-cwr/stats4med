7510058a034eb0030b1887cec92c41b7da69e963
Eduardo Junior <edujrrib@gmail.com>
2016-08-18 13:53:52 -0300

    Add slides COM-Poisson into navbar and modify build travis with seminars directory

--- _deploy.sh
+++ _deploy.sh
@@ -6,7 +6,7 @@ git config --global user.name "Travis boot"
 git clone -b gh-pages https://${GIT_KEY}@github.com/${TRAVIS_REPO_SLUG}.git dir-tmp
 cd dir-tmp
 cp -r ../course/_site/* ./
-cp -r ../seminars/*.pdf ./
+cp -r ../seminars/*.pdf ./seminars/
 git add --all *
 git commit -m "Atualização automática (travis build ${TRAVIS_BUILD_NUMBER})" || true
 git push origin gh-pages
